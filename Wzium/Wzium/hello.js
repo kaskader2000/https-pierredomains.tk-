3dc0682d69e4bea0d37826277a5a19b0f99972d2
kaskader2000 <65952276+kaskader2000@users.noreply.github.com>
2020-12-29 16:20:50 +0100

    Delete hello.js

--- Wzium/Wzium/hello.js
+++
@@ -1,7 +0,0 @@
-const name = prompt("Podaj swój nick");
-
-if (name) {
-    alert("Hejka, widzę, że chcesz powziumować, " + name);
-} else {
-    alert("Hejka, widzę że lubisz wziumy i znasz je dobrze, " + name);
-}
