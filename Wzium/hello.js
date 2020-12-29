8cec6fde5650e932245e1b3daf0d20f4bdc2dd8b
kaskader2000 <65952276+kaskader2000@users.noreply.github.com>
2020-12-29 13:29:45 +0100

    Add files via upload

---
+++ Wzium/hello.js
@@ -0,0 +1,7 @@
+const name = prompt("Podaj swój nick");
+
+if (name) {
+    alert("Hejka, widzę, że chcesz powziumować, " + name);
+} else {
+    alert("Hejka, widzę że lubisz wziumy i znasz je dobrze, " + name);
+}
